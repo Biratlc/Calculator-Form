6435d1f9124e89bf6b68c991bb89c82f2509143a
Birat Lamichhane <birat.lamichhane@selu.edu>
2019-06-06 15:49:27 -0500

    .Net Web form with button property setup

---
+++ Calculator/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub txtMessage_Click(sender As Object, e As EventArgs) Handles txtMessage.Click
+        txtName.Text = "Hello" & txtName.Text
+
+    End Sub
+
+
+End Class
---
+++ Calculator/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Calculator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Calculator")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b7b6a75b-6be5-49a2-98fe-c538e1375335")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
